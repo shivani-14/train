f21f2e7284253823663d3c4599d1dfddb2a96e40
Aleti Shivani Reddy <shivanireddy0214@gmail.com>
2020-10-26 00:52:00 +0530

    adding day2 files

---
+++ Examples/myscript.js
@@ -0,0 +1,6 @@
+		 function submitValues(){
+		 	var num1=parseInt(document.getElementById('txt1').value);
+		 	var num2=parseInt(document.getElementById('txt2').value);
+
+		 	alert("addition of two number is"+(num1+num2))
+		 }
